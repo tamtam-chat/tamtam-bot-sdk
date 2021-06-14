d2295dbcdb4d294e4c1eb0bd2787357703ec9649
almaz.shakirov <almaz.shakirov@corp.mail.ru>
2021-06-14 19:03:35 +0300

    Added doc for UpdateHandler

--- src/main/java/chat/tamtam/bot/annotations/UpdateHandler.java
+++ src/main/java/chat/tamtam/bot/annotations/UpdateHandler.java
@@ -6,7 +6,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Indicates that an annotated method is an "update handler".
+ * Such methods are considered as candidates for auto-detection
+ * when their classes added as "handler" to {@link chat.tamtam.bot.TamTamBotBase TamTamBotBase} or its inheritors.
+ * An annotated method must have one parameter with type {@link chat.tamtam.botapi.model.Update Update}
+ * or its inheritor in the method definition.
+ *
  * @author alexandrchuprin
+ * @see chat.tamtam.bot.TamTamBotBase
+ * @see chat.tamtam.botapi.model.Update
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
