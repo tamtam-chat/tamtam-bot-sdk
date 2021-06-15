2d980106287543561221d0b8466e4c6ffa594783
almaz.shakirov <almaz.shakirov@corp.mail.ru>
2021-06-15 12:41:02 +0300

    fix

--- src/main/java/chat/tamtam/bot/annotations/UpdateHandler.java
+++ src/main/java/chat/tamtam/bot/annotations/UpdateHandler.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * Indicates that an annotated method handles incoming updates.
  * Such methods are considered as candidates for auto-detection
  * when its containing class added as "handler" to {@link chat.tamtam.bot.TamTamBotBase TamTamBotBase} or its inheritors.
- * An annotated method must have one parameter with inherited type {@link chat.tamtam.botapi.model.Update Update}
+ * An annotated method must have one parameter with concrete implementation of {@link chat.tamtam.botapi.model.Update Update}
  * in the method definition.
  *
  * @author alexandrchuprin
